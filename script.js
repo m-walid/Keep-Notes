18680bd944fd7d62d1ddc67f6841409cb90c596d
walid <mohamed.walid1999@gmail.com>
2020-07-25 14:15:00 +0200

    added desktop drag function

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ function saveChanges() {
   if (openCardText === false) {
     const card = document.createElement("div");
     card.addEventListener("click", openNote);
+    cardDragEventsHandler(card);
     card.classList.add("card");
+    card.setAttribute("draggable", "true");
     card.innerHTML = `<i class="fa fa-trash remove" aria-hidden="true"></i>
       <p>${noteText.value}</p>`;
     cardsContainer.insertBefore(card, cardsContainer.firstElementChild);
@@ -47,7 +49,6 @@ function closeOverlay() {
       overlay.style.opacity = Number(overlay.style.opacity) - 0.01;
     } else {
       overlay.classList.add("hide");
-      note.classList.add("hide");
       clearInterval(hide);
     }
   }, 3);
@@ -59,7 +60,6 @@ function openOverlay() {
   note.style.opacity = 1;
   overlay.style.opacity = 1;
   overlay.classList.remove("hide");
-  note.classList.remove("hide");
 }
 
 function savetoStorage() {
@@ -76,7 +76,9 @@ function loadFromStorage() {
     notes.forEach((note) => {
       const card = document.createElement("div");
       card.addEventListener("click", openNote);
+      cardDragEventsHandler(card);
       card.classList.add("card");
+      card.setAttribute("draggable", "true");
       card.innerHTML = `<i class="fa fa-trash remove" aria-hidden="true"></i>
       <p>${note}</p>`;
       cardsContainer.appendChild(card);
@@ -92,3 +94,68 @@ addBt.addEventListener("click", openOverlay);
 cards.forEach((card) => card.addEventListener("click", openNote));
 
 loadFromStorage();
+
+function dragStart(card) {
+  card.classList.add("dragging");
+  setTimeout(() => {
+    card.style.opacity = "0.2";
+  }, 1);
+}
+
+function dragEnd(card) {
+  card.classList.remove("dragging");
+  card.style.opacity = "1";
+  savetoStorage();
+}
+
+function dragOver(e, card) {
+  e.preventDefault();
+  const draggable = document.querySelector(".dragging");
+  if (cardsContainer.lastElementChild === card) {
+    cardsContainer.append(draggable);
+  } else if (draggable.nextElementSibling === card) {
+    cardsContainer.insertBefore(card, draggable);
+  } else {
+    cardsContainer.insertBefore(draggable, card);
+  }
+}
+cards.forEach((card) => {
+  cardDragEventsHandler(card);
+});
+
+function cardDragEventsHandler(card) {
+  card.addEventListener("dragstart", () => dragStart(card));
+  card.addEventListener("dragend", () => dragEnd(card));
+  card.addEventListener("dragover", (e) => dragOver(e, card));
+  // card.addEventListener("touchstart", () => dragStart(card));
+  // card.addEventListener("touchend", () => dragEnd(card));
+  // card.addEventListener("touchmove", (e) => dragOver(e, card));
+}
+
+// cardsContainer.addEventListener('dragover', e =>{
+//   e.preventDefault();
+//   const draggable = document.querySelector('.dragging');
+//   const cardAfter = getClosestCard(e.clientX,e.clientY);
+//   if(typeof cardAfter.elm != 'undefined'){
+//     cardsContainer.insertBefore(draggable,cardAfter.elm);
+//   }
+//   else{
+//     cardsContainer.appendChild(draggable);
+//   }
+
+// });
+
+// function getClosestCard(x,y){
+
+//   const cards = document.querySelectorAll('.card');
+//   let closest = {x: Number.NEGATIVE_INFINITY, y: Number.POSITIVE_INFINITY};
+//   for(card of cards){
+//      const boundries = card.getBoundingClientRect();
+//      const offset = {x: x - boundries.left - boundries.width / 2 ,  y: Math.abs(y - boundries.top - boundries.height / 2) };
+//      if(offset.x < 0 && offset.y < closest.y && offset.x >= closest.x){
+//        closest = {offset : offset , elm:card};
+//      }
+//   }
+//   console.log(closest.offset)
+//   return closest
+// }
